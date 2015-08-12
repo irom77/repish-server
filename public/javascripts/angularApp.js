875c579e762a2a65aec4fa2a6e99019c2de054ae
irom77 <aniairom0>
2015-08-12 16:01:02 -0400

    all forms except guest wireless completed and tested

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -3,15 +3,17 @@
  */
 var app = angular.module("angularApp", [])
 .controller("myConfigGenCtrl", function($scope, $http, customService) {
-    //customService.getData().then(function(response) {
-    //console.log(response.data);
-    //   $scope.myTextArea = response.data;
-    //});
+    $scope.wan={addr:'dhcp'};
+    $scope.ssid= {guest: true};
     $scope.save  = function(data, filename) {
-        data = $("#textarea").val().replace(/\n/g, '\r\n');
+        data = $("#textarea").val();
+        if ($scope.wan.addr != 'static') data = data.replace(/.*WAN ipv4-address.*/g, '');
+        if ($scope.wan.addr != 'dhcp') data = data.replace(/.*WAN type dhcp.*/g, '');
+        if ($scope.wan.addr != 'pppoe') data = data.replace(/.*WAN type pppoe.*/g, '');
+        //if (!$scope.ssid.guest) data = data.replace(/.*#Guest.*+5d/g, '');
+        data = data.replace(/\n/g, '\r\n');
         var blob = new Blob([data], {type: "text/plain;charset=utf-8"});
         filename = "autoconf.clish";
-        //console.log(data);
         saveAs(blob, filename);};
 })
 .service ('customService',function($http) {
