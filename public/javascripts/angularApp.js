f4ba634bfabdc83af388eaa2075fcb486e632666
irom77 <aniairom0>
2015-08-13 07:58:30 -0400

    guest wireless regex fixed

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -4,13 +4,14 @@
 var app = angular.module("angularApp", [])
 .controller("myConfigGenCtrl", function($scope, $http, customService) {
     $scope.wan={addr:'dhcp'};
-    $scope.ssid= {guest: true};
+    $scope.ssid= {guest: 1};
     $scope.save  = function(data, filename) {
         data = $("#textarea").val();
         if ($scope.wan.addr != 'static') data = data.replace(/.*WAN ipv4-address.*/g, '');
         if ($scope.wan.addr != 'dhcp') data = data.replace(/.*WAN type dhcp.*/g, '');
         if ($scope.wan.addr != 'pppoe') data = data.replace(/.*WAN type pppoe.*/g, '');
-        //if (!$scope.ssid.guest) data = data.replace(/.*#Guest.*+5d/g, '');
+        //https://regex101.com/r/rO0yD8/13
+        if ($scope.ssid.guest==0) data = data.replace(/#Guest.*(?:\n.*){7}/g,'');
         data = data.replace(/\n/g, '\r\n');
         var blob = new Blob([data], {type: "text/plain;charset=utf-8"});
         filename = "autoconf.clish";
