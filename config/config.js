739d098f14927fbba6533b4803a1f4a02e0843b2
irom77 <aniairom0>
2015-08-17 12:07:05 -0400

    fixing .gitignore

--- config/config.js
+++ config/config.js
@@ -1,1 +1,11 @@
-ignored
+module.exports = {
+    repSic: 'passwordSic',
+    CA: 'internal_ca',
+    user_host: 'repvpnuser@provider1w.commonwealth.com',
+    resetSic: 'LSMcli 10.29.21.237 CPService IHateMundays1! ResetSic ',
+    resetIke: 'LSMcli 10.29.21.237 CPService IHateMundays1! ResetIke ',
+    envCMA: 'mdsenv repvpnsc1w;',
+    ActivationKey: ' CFNs1cC0nn3ct',
+    lscertIKE:    'cpca_client lscert -stat Valid -kind IKE | grep -A 2 ',
+    lscertSIC:    'cpca_client lscert -kind SIC | grep -A 2 '
+};
