ca75512291e10036bd1c6e107aad4018ada5f259
irom77 <aniairom0>
2015-08-19 16:24:39 -0400

    cli-repcertifcates added

---
+++ modules/cli-repcertifcates.js
@@ -0,0 +1,34 @@
+/**
+ * Created by irekromaniuk on 8/19/2015.
+ */
+var exec = require('ssh-exec');
+var config = require('./../configure/config');
+
+RoboName = process.argv[2]; //'Irek_Test_1100';
+//command = config.envCMA + config.resetSic + RoboName + config.ActivationKey + ';' + config.lscertSIC + RoboName + ';';
+command = config.envCMA + config.lscertSIC + RoboName + ';';
+host = config.user_host;
+console.log(command + '\n' + host);
+if (!RoboName) {
+    console.log('RoboName is empty');
+    process.exit();
+}
+output = exec(command, host).pipe(process.stdout);
+return(output);
+/*
+ var buffers = [];
+ var reset = function (command) {
+ stream = process.stdin
+ .pipe(exec(command, host));
+ stream.on('data', function (buffer) {
+ buffers.push(buffer);
+ });
+ stream.on('end', function () {
+ var buffer = Buffer.concat(buffers);
+ //console.log(buffer.toString());
+ return(buffer.toString());
+ //process.stdin.end();
+ });
+ };
+*/
+ reset(command);
