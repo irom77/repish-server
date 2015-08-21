1f81779002f6ce1680812943906bee6590bf9e60
irom77 <aniairom0>
2015-08-21 15:28:58 -0400

    req.RoboName testing

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,10 @@ router.get('/', function(req, res,next) {
   res.render('index');
 });
 
-router.get('/api/manage', function(req, res, next) {
+router.post('/api/manage', function(req, res, next) {
   //res.render('test');
   //res.json({name: 'foo'});
-  data = repcertificates('Irek_Test_1100');//TESTED SUCCESS
+  data = repcertificates(req.RoboName);//TESTED SUCCESS 'Irek_Test_1100''
   //data = 'TESTing';
   //console.log(repcertificates('Irek_Test_1100'));
   /*res.writeHead(200, {
