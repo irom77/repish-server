6230f11ddbb34859d370a08fd5f97177375b6b5d
irom77 <aniairom0>
2015-08-21 15:58:19 -0400

    req.body.RoboName testing

--- routes/index.js
+++ routes/index.js
@@ -11,14 +11,15 @@ router.post('/api/manage', function(req, res, next) {
   //res.render('test');
   //res.json({name: 'foo'});
   // data = req.body.RoboName;
-// data = repcertificates(req.body.RoboName);//TESTED SUCCESS 'Irek_Test_1100', API_Test_1100
+//data = repcertificates(req.body.RoboName);//TESTED SUCCESS 'Irek_Test_1100', API_Test_1100
+  data = "Under Development";
   //data = 'TESTing';
   //console.log(repcertificates('Irek_Test_1100'));
   /*res.writeHead(200, {
     'Content-Type': 'text/html',
     'Access-Control-Allow-Origin': '*'
   });*/
-//res.send(data);
+  res.send(data);
   //verify with 'cpca_client lscert -kind SIC -stat Pending | grep -A 2 RoboName'
 });
 
